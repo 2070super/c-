09890546c12942ff8f35b88ef46c41d9f0891b2b
2070super <1025404072@qq.com>
2022-02-17 22:49:32 +0800

    输入输出

---
+++ day1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+int main() {
+	cout << "hello world" << endl;
+	std::cout << "hello world" << endl;
+	int i;
+	char ch;
+	double c;
+	std::cin >> i;
+	std::cin >> ch >> c;
+	std::cout << i << "--" << ch << "--" << c;
+	
+
+}
