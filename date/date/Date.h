c4606f15b799e5a61a418237ee01c249630b6ffe
2070super <1025404072@qq.com>
2023-01-07 00:27:48 +0800

    日期的计算工具

---
+++ date/date/Date.h
@@ -0,0 +1,29 @@
+class Date
+{
+public:
+	int GetMonthDay(int year, int month);
+	void print();
+	Date(int year = 1900, int month = 1, int day = 1);
+	Date& operator+=(int day);
+	Date(const Date& d);
+	Date operator+(int day);
+	bool operator>(const Date& d);
+	bool operator < (const Date& d);
+	bool operator <= (const Date& d);
+	Date& operator-=(int day);
+	Date& operator++();
+	Date operator++(int);
+	Date& operator--();
+	Date operator--(int);
+	bool operator != (const Date& d);
+	bool operator == (const Date& d);
+	int operator-(const Date& d);
+	bool operator >= (const Date& d);
+	Date operator-(int day);
+	Date& operator=(const Date& d);
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
