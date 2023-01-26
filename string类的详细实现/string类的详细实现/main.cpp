73c5ae3cda9e37236eb72425895be945300cd90f
2070super <1025404072@qq.com>
2023-01-26 19:56:28 +0800

    string类的详细实现

---
+++ string类的详细实现/string类的详细实现/main.cpp
@@ -0,0 +1,7 @@
+#include"string.h"
+using namespace std;
+int main()
+{
+	haha::string s1("hello");
+	haha::string s2(s1);
+}
---
+++ string类的详细实现/string类的详细实现/string.h
@@ -0,0 +1,45 @@
+#pragma once
+#include<iostream>
+#include <cassert>
+#include<algorithm>
+namespace haha
+{
+	class string
+	{
+	public:
+		~string();
+		string();
+		typedef char* iterator;
+		iterator begin()//暂时不知道如何将该方法写入cpp
+		{
+			return _str;
+		}
+		iterator end()
+		{
+			return _str + _size;
+		}
+		iterator begin() const//暂时不知道如何将该方法写入cpp
+		{
+			return _str;
+		}
+		iterator end() const
+		{
+			return _str + _size;
+		}
+		string(const char* str);
+		size_t size();
+		char& operator[](size_t t);
+		void push_back(char ch);
+		void append(const char* ch);
+		void reserve(size_t t);
+		string& operator+=(char ch);
+		string& operator+=(const string& ch);
+		string& operator+=(const char* ch);
+		string(const string& s);
+		void swap(char* a, char* b);
+	private:
+		char* _str;
+		size_t _size;
+		size_t _capacity;
+	};
+}
