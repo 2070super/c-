e9ee9b947525cfae5ae62da694f7c7522d36836b
2070super <1025404072@qq.com>
2023-03-25 23:26:23 +0800

    更新

--- 搜索二叉树/搜索二叉树/BinarySearchTree.h
+++ 搜索二叉树/搜索二叉树/BinarySearchTree.h
@@ -77,6 +77,81 @@ public:
 			_InOrder(root->_right);
 		}
 	}
+	void erase(int value)
+	{
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_key < key)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_key > key)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}//查找完成
+			if (cur->_left == nullptr)//左子树为空
+			{
+				if (cur == root)//只有右子树
+				{
+					_root = cur->_right;
+				}
+				else
+				{
+					if (cur == parent->_left)
+					{
+						parent->_left = cur->_right;
+					}
+					else
+					{
+						parent->_right == cur->_left;
+					}
+				}
+			}
+			else if (cur->_right == nullptr)
+			{
+				if (_root = cur)
+				{
+					_root = cur->_left;
+				}
+				else 
+				{
+					if (cur == parent->_left)
+					{
+						parent->_left = cur->_right;
+					}
+					else
+					{
+						parent->_right = cur->_right;
+					}
+				}
+			}
+			else
+			{
+				Node* minparent = nullptr;
+				Node* min = cur->_right;
+				while (min->_left)
+				{
+					minparent = min;
+					min = min->_left;
+				}
+				swap(cur->_key, min->_key);
+				if (minparent->_left = min)
+				{
+					minparent->_left = min->_right;
+				}
+				else
+				{
+					minparent->_right = min->_right;
+				}
+				minparent->_left = min->_right;
+				delete min;
+			}
+		}
+	}
 private:
 	Node* _root;
 };
